c877333fe9704a7c6a02b15f48c87dba43ac4fda
Sagar Khatri <sagar@zentrumhub.com>
2024-09-16 22:42:10 +0530

    gh-pages trial v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Enables static export
+    basePath: '/fire-home', // Replace with your repository name
+    assetPrefix: '/fire-home',
+};
 
 export default nextConfig;
