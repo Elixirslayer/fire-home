db389dbd6a9fad02f9fa5f16e021cd20695eb0e7
Sagar Khatri <sagar@zentrumhub.com>
2024-09-17 21:52:12 +0530

    fix path string

--- src/app/components/SiteComponent.tsx
+++ src/app/components/SiteComponent.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { SiteData } from "@/app/models/SiteData";
 import Image from "next/image";
 import { repoBasePath } from "../Constants";
+import { useEffect } from "react";
 
 function SiteComponent({siteData}: {siteData :SiteData}) {
     const isGhDeployment: boolean = process.env.NODE_ENV === "production";
@@ -8,11 +10,16 @@ function SiteComponent({siteData}: {siteData :SiteData}) {
     function redirect(){      
         window.open(siteData.RedirectUrl, '_blank');
     }
+
+    useEffect(()=>{
+        console.log(((isGhDeployment ? basePath : "") + siteData.ImageUrl));
+    }, [isGhDeployment, basePath, siteData.ImageUrl])
+
     return ( 
         <>
             <div className="flex flex-col items-center hover:cursor-pointer" onClick={redirect}>
                 <div className="rounded-xl border w-56 h-56 p-5 hover:p-1 transition-all duration-300 overflow-hidden flex">
-                    <Image className="my-auto" src={isGhDeployment ? basePath : "" + siteData.ImageUrl} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
+                    <Image className="my-auto" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
                 </div>
                 <div className="font-bold text-xl my-4">
                     {siteData.Title}
