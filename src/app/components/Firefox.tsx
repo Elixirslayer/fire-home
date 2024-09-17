b74c3c429d35f3d7ab7632af048c5cb0d61715bd
Sagar Khatri <sagar@zentrumhub.com>
2024-09-17 21:58:57 +0530

    removed debug code

--- src/app/components/Firefox.tsx
+++ src/app/components/Firefox.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import { repoBasePath } from "../Constants";
 
 function Firefox() {
+    const isGhDeployment: boolean = process.env.NODE_ENV === "production";
+    const basePath: string = repoBasePath;
+
     return ( 
         <>
             <div className="flex">
                 <div className="m-2">
-                    <Image src="/images/firefox.png" alt="firefox" height={150} width={150}></Image>
+                    <Image src={((isGhDeployment ? basePath : "") + "/images/firefox.png")} alt="firefox" height={150} width={150}></Image>
                 </div>
                 <div className="m-2 flex items-center">
                     <h1 className="text-5xl font-bold">Firefox</h1>
