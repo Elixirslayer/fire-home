996f86eb36ca8b0f0c4d6898dfc5e508e61cb571
Sagar Khatri <sagar@zentrumhub.com>
2024-09-18 08:34:52 +0530

    rounded images to match rounded divs

--- src/app/components/SiteComponent.tsx
+++ src/app/components/SiteComponent.tsx
@@ -19,7 +19,7 @@ function SiteComponent({siteData}: {siteData :SiteData}) {
         <>
             <div className="flex flex-col items-center hover:cursor-pointer" onClick={redirect}>
                 <div className="rounded-xl border w-56 h-56 p-5 hover:p-1 transition-all duration-300 overflow-hidden flex">
-                    <Image className="my-auto" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
+                    <Image className="my-auto rounded-xl" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
                 </div>
                 <div className="font-bold text-xl my-4">
                     {siteData.Title}
