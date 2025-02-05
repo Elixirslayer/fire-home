a977fdd61b8f68ee31b130c3b5568ffcc2a95698
SagarOP <sagar@zentrumhub.com>
2025-02-06 00:15:24 +0530

    added persisting background image with toggle switch

--- src/app/components/SiteComponent.tsx
+++ src/app/components/SiteComponent.tsx
@@ -3,24 +3,22 @@ import { SiteData } from "@/app/models/SiteData";
 import Image from "next/image";
 import { repoBasePath } from "../Constants";
 
-function SiteComponent({siteData}: {siteData :SiteData}) {
+function SiteComponent({ siteData }: { siteData: SiteData }) {
     const isGhDeployment: boolean = process.env.NODE_ENV === "production";
     const basePath: string = repoBasePath;
-    function redirect(){      
+    function redirect() {
         window.open(siteData.RedirectUrl, '_blank');
     }
 
-    return ( 
-        <>
-            <div className="flex flex-col items-center hover:cursor-pointer" onClick={redirect}>
-                <div className="rounded-xl border w-56 h-56 p-5 hover:p-1 transition-all duration-300 overflow-hidden flex">
-                    <Image className="my-auto rounded-xl" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
-                </div>
-                <div className="font-bold text-xl my-4">
-                    {siteData.Title}
-                </div>
+    return (
+        <div className="flex flex-col items-center hover:cursor-pointer" onClick={redirect}>
+            <div className="rounded-xl border w-56 h-56 p-5 hover:p-1 bg-black bg-opacity-50 transition-all duration-300 overflow-hidden flex">
+                <Image className="my-auto rounded-xl" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
             </div>
-        </>
+            <div className="font-bold text-xl my-4">
+                {siteData.Title}
+            </div>
+        </div>
     );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,66 +6,76 @@ import Firefox from "./components/Firefox";
 import { repoBasePath, sites } from "./Constants";
 
 export default function Home() {
-    const isGhDeployment: boolean = process.env.NODE_ENV === "production";
-    const basePath: string = repoBasePath;
-
     const [isAnime, setIsAnime] = useState<boolean>(false);
     const [translateX, setTranslateX] = useState<string>("0px");
-    // const [buttonImage, setButtonImage] = useState("");
-
-    // function getButtonImage(): string{
-    //     // const imageBaseUrl: string = "/images/ichigo.png";
-    //     // console.log("here");
-        
-    //     // if(isGhDeployment){
-    //     //     console.log(`bg-[url('${basePath + imageBaseUrl}')]`);
-    //     //     return `bg-[url('${basePath + imageBaseUrl}')]`;
-    //     // }
-    //     // console.log(`bg-[url('${imageBaseUrl}')]`);
-    //     // return `bg-[url('${imageBaseUrl}')]`;
-
-    //     return "/images/ichigo.png";
-    // }
-
+    const isGhDeployment: boolean = process.env.NODE_ENV === "production";
+    const basePath: string = repoBasePath;
+    const buttonImage = getSliderImage();
+    const backgroundImage = getBackgroundImage();
 
     function changeTheme() {
-        console.log(!isAnime ? "2.5rem" : "0px");
+        localStorage.setItem("isAnime", JSON.stringify(!isAnime));
         setTranslateX(!isAnime ? "2.5rem" : "0px");
         setIsAnime(!isAnime);
     }
 
-    // useEffect(()=>{
-    //     setButtonImage(getButtonImage());
-    // }, [])
+    function getSliderImage(): string {
+        const imageUrl = "/images/ichigo.jpg";
+        if (isGhDeployment)
+            return `bg-[url(${basePath + imageUrl})]`;
+        return "bg-[url(/images/ichigo.jpg)]";
+    }
+
+    function getBackgroundImage(): string {
+        const imageUrl = "/images/gotei-4k.jpg";
+        if (isGhDeployment)
+            return `bg-[url(${basePath + imageUrl})]`;
+        return "bg-[url(/images/gotei-4k.jpg)]";
+    }
+
+    useEffect(() => {
+        const persistThemeStr = localStorage.getItem("isAnime");
+        if (persistThemeStr) {
+            const persistTheme = JSON.parse(persistThemeStr);
+            console.log(persistTheme);
+            
+            if (persistTheme) {
+                setIsAnime(true);
+                setTranslateX('2.5rem');
+            }
+            else {
+                setIsAnime(false);
+                setTranslateX('0px');
+            }
+        }
+    }, [])
 
     return (
-        <>
-            <div className="w-full flex flex-col items-center p-8">
-                <div className="m-2 mb-16 w-full flex justify-evenly">
-                    <div className="w-1/3"></div>
+        <div className={`w-full flex flex-col items-center p-8 bg-cover bg-center ${isAnime ? backgroundImage : "bg-black"}`}>
+            <div className="m-2 mb-16 w-full flex justify-evenly">
+                <div className="w-1/3"></div>
 
-                    <div className="w-1/3 flex justify-center">
-                        <Firefox></Firefox>
-                    </div>
-
-                    <div className="flex w-1/3 items-center justify-end px-12">
-                        <button className="rounded-full bg-zinc-50 text-black p-1 w-[5.5rem]" onClick={changeTheme}>
-                            <div className={`rounded-[5rem] w-10 h-10 transition-all duration-150 `} style={{ transform: `translateX(${translateX})` }}>
-
-                            </div>
-                        </button>
-                    </div>
+                <div className="w-1/3 flex justify-center">
+                    <Firefox></Firefox>
                 </div>
-                <div className="flex max-w-screen-2xl flex-wrap justify-center">
-                    {sites.map((site) => {
-                        return (
-                            <div key={site.Title} className="m-4">
-                                <SiteComponent siteData={site}></SiteComponent>
-                            </div>
-                        );
-                    })}
+
+                <div className="flex w-1/3 items-center justify-end px-12">
+                    <button className="rounded-full bg-zinc-50 text-black p-1 w-[5.5rem]" onClick={changeTheme}>
+                        <div className={`rounded-[5rem] w-10 h-10 transition-all duration-150 bg-cover bg-center ${isAnime ? buttonImage : "bg-black"}`} style={{ transform: `translateX(${translateX})` }}>
+
+                        </div>
+                    </button>
                 </div>
             </div>
-        </>
+            <div className="flex max-w-screen-2xl flex-wrap justify-center">
+                {sites.map((site) => {
+                    return (
+                        <div key={site.Title} className="m-4">
+                            <SiteComponent siteData={site}></SiteComponent>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
